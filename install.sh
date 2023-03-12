34f736d8de5298e9f8d4d911379e7d80078eb4c0
Carlos Canicio <canicio7@gmail.com>
2023-03-12 15:34:38 +0100

    Script to generate binarie and install it

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ -f "./build" ]]; then
+    rm -rf ./build
+fi
+
+if [[ -f "./dist/utilitiestool" ]]; then
+    rm -rf ./dist/utilitiestool
+fi
+venv/bin/pyinstaller -D -F -n utilitiestool -c main.py --key TdQeHy_37dpg%erN7
+if [[ -f "/usr/local/bin/utilitiestool" ]]; then
+    rm -rf /usr/local/bin/utilitiestool
+fi
+cp dist/utilitiestool /usr/local/bin/utilitiestool
+if [[ -f "/usr/local/bin/utilitiestool" ]]; then
+    chmod 755 /usr/local/bin/utilitiestool
+fi
